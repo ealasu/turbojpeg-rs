facd337e4af1acb1080a8b8b17a41f86226326a7
eman <e@m.an>
2018-04-21 09:57:31 -0700

    fix

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate pkg_config;
 
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("1.5.0")
+        .atleast_version("1.3.0")
         .statik(true)
         .probe("libturbojpeg")
         .unwrap();
